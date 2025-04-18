332e26f7abb8155ea7b0e412a1d37fc1e1ca220f
norimicry <46500116+norimicry@users.noreply.github.com>
2025-04-17 20:35:00 -0400

    Update cron setup script to include environment variables

--- cron.setup.sh
+++ cron.setup.sh
@@ -4,7 +4,7 @@ apt-get update && apt-get install -y cron logrotate
 mkdir -p /var/log/ynab-sync
 
 cat <<EOF > /etc/cron.d/ynab-sync
-0 * * * * /usr/local/bin/python /app/main.py >> /var/log/ynab-sync/sync.log 2>&1
+0 * * * * SOURCE_API_KEY=$SOURCE_API_KEY TARGET_API_KEY=$TARGET_API_KEY SOURCE_BUDGET_ID=$SOURCE_BUDGET_ID TARGET_BUDGET_ID=$TARGET_BUDGET_ID SHARED_CATEGORY_ID=$SHARED_CATEGORY_ID SHARED_ACCOUNT_ID=$SHARED_ACCOUNT_ID /usr/local/bin/python3 /app/main.py >> /var/log/ynab-sync/sync.log 2>&1
 EOF
 
 chmod 0644 /etc/cron.d/ynab-sync
